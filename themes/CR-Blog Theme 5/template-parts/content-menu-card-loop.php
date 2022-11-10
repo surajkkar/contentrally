8ab6786e7a50fa8352f3ce8bdb034a10d6060be0
sharmita95 <viacon.sharmita@gmail.com>
2022-11-10 13:16:32 +0530

    header menu issue fixed

--- themes/CR-Blog Theme 5/template-parts/content-menu-card-loop.php
+++ themes/CR-Blog Theme 5/template-parts/content-menu-card-loop.php
@@ -2,6 +2,6 @@
 <a class="small-card-sec" href="<?php echo get_the_permalink($postid); ?>">
 	<?php echo get_the_post_thumbnail( $postid , 'main-thumbnail' ,array( 'class' => 'card-image' )); ?>
 	<h6 class="small-c-titel">
-		<?php get_limited_title_func($post->post_title, 40); ?>
+		<?php get_limited_title_func(strip_tags($post->post_title), 40); ?>
 	</h6>
 </a>
